fffadc4c570fe17bfaee1f5f2054fb075ced3c95
nuts-n-bits <civ6@icloud.com>
2024-12-23 17:44:34 -0500

    +work

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-
     let argv = std::env::args().collect::<Vec<String>>();
     let first_arg = argv.get(1).map(|s| s.as_str());
     match first_arg {
@@ -21,43 +20,50 @@ async fn main() {
 async fn run_server() {
     // Bind the listener to the address
     let listener = tokio::net::TcpListener::bind("::0:9098").await.unwrap();
-    
+
     loop {
         // The second item contains the IP and port of the new connection.
         let (socket, _) = listener.accept().await.unwrap();
         process(socket).await;
     }
-    
 }
 
 async fn process(mut socket: tokio::net::TcpStream) {
     let addr = socket.peer_addr().unwrap();
     println!("connected to {:?}", addr);
-    let mut buf = [0u8; 8];
+    let mut buf = [0u8; 1024];
     loop {
-        buf = [0u8; 8];
-        let read = socket.read(&mut buf[..]).await;
+        buf = [0u8; 1024];
+        let read = socket.read_exact(&mut buf[..]).await;
         dbg!(&read);
         dbg!(std::str::from_utf8(&buf[..]));
 
-        let write = socket.write_all(&buf).await;
-        match write {
-            Err(_) => { break; },
-            Ok(_) => {},
-        }
-
-        if read.unwrap_or(0) == 0 { 
+        if read.unwrap_or(0) == 0 {
             socket.flush();
             std::mem::drop(socket);
             println!("socket {:?} will be dropped", addr);
-            break; 
+            break;
         }
-    }
 
+        let returnable = work(&buf);
+
+
+        let write = socket.write_all(&buf).await;
+        match write {
+            Err(_) => {
+                break;
+            }
+            Ok(_) => {}
+        }
+
+
+    }
 }
 
 async fn run_client() {
-    let mut stream = tokio::net::TcpStream::connect("127.0.0.1:9098").await.unwrap();
+    let mut stream = tokio::net::TcpStream::connect("127.0.0.1:9098")
+        .await
+        .unwrap();
     let write = stream.write("afrwahgreggreshtrshtrhtrdhtrdhtrhthtresdgtrhtrdhtrsh".as_bytes());
     dbg!(write);
 }
@@ -65,12 +71,14 @@ async fn run_client() {
 const PI: f64 = 3.14;
 const E: f64 = 2.71;
 
-fn work(incoming_bytes: [u8; 1024]) -> [f64; 1024] {
-        let result = [0f64; 1024];
-        for (i, elem) in incoming_bytes.iter().enumerate() {
-                
+fn work(incoming_bytes: &[u8; 1024]) -> [f64; 1024] {
+    let mut result = [0f64; 1024];
+    for (i, elem) in incoming_bytes.iter().enumerate() {
+        if i & 0b1 == 0 {
+            result[i] = (*elem as f64) * PI;
+        } else {
+            result[i] = (*elem as f64) / E;
         }
-
-        ;
-
+    }
+    result
 }
