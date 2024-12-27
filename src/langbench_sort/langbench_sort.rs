ad400fc0c2e8e31b6185f8c621ddb020118f7d8b
nnb <civ6@icloud.com>
2024-12-26 20:16:22 -0500

    re-port sort

--- src/langbench_sort/langbench_sort.rs
+++ src/langbench_sort/langbench_sort.rs
@@ -1,125 +1,125 @@
 
-fn assert(cond: bool) {
-	if !cond {
-		panic!("badness");
-	}
+
+fn swap(arr []string, lo: usize, hi: usize) {
+	var tmp string = arr[lo]
+	arr[lo] = arr[hi]
+	arr[hi] = tmp
 }
 
-fn wmerge(arr: &mut Vec<String>, mut lo1: usize, hi1: usize,  mut lo2: usize, hi2: usize, mut w: usize) {
-	while lo1 < hi1 && lo2 < hi2 {
-		let mut lo_old = 0;
+fn wmerge(arr []string, lo1: usize, hi1: usize, lo2: usize, hi2: usize, w: usize) {
+	for ((lo1 < hi1) && (lo2 < hi2)) {
+		var lo_old int
 		if arr[lo1] <= arr[lo2] {
-			lo_old = lo1;
-			lo1 += 1;
+			lo_old = lo1
+			lo1++
 		} else {
-			lo_old = lo2;
-			lo2 += 1;
+			lo_old = lo2
+			lo2++
 		}
-		let w_old = w;
-		w += 1;
-		arr.swap(w_old, lo_old);
+		var w_old int = w
+		w++
+		swap(arr, w_old, lo_old)
 	}
-	while lo1 < hi1 {
-		let w_old = w;
-		w += 1;
-		let lo_old = lo1;
-		lo1 += 1;
-		arr.swap(w_old, lo_old);
+	for (lo1 < hi1) {
+		var w_old int = w
+		w++
+		var lo_old int = lo1
+		lo1++
+		swap(arr, w_old, lo_old)
 	}
-	while lo2 < hi2 {
-		let w_old = w;
-		w += 1;
-		let lo_old = lo2;
-		lo2 += 1;
-		arr.swap(w_old, lo_old);
-	}
-}
-
-fn wsort(arr: &mut Vec<String>, lo: usize, hi: usize, w: usize) {
-	if hi - lo > 1 {
-		let m = (lo + hi) / 2;
-		imsort(arr, lo, m);
-		imsort(arr, m, hi);
-		wmerge(arr, lo, m, m, hi, w);
-	} else if lo != hi {
-		arr.swap(lo, w)
+	for (lo2 < hi2) {
+		var w_old int = w
+		w++
+		var lo_old int = lo2
+		lo2++
+		swap(arr, w_old, lo_old)
 	}
 }
 
-fn imsort(arr: &mut Vec<String>, lo: usize, hi: usize) {
+fn wsort(arr []string, lo: usize, hi: usize, w: usize) {
 	if (hi - lo) > 1 {
-		let m = (lo + hi) / 2;
-		let mut w = lo + hi - m;
-		wsort(arr, lo, m, w);
-		while w - lo > 2 {
-			let n = w;
-			w = (lo + n + 1) / 2;
-			wsort(arr, w, n, lo);
-			wmerge(arr, lo, lo + n - w, n, hi, w);
+		var m int = (lo + hi) / 2
+		imsort(arr, lo, m)
+		imsort(arr, m, hi)
+		wmerge(arr, lo, m, m, hi, w)
+	} else if lo != hi {
+		swap(arr, lo, w)
+	}
+}
+
+fn imsort(arr []string, lo: usize, hi: usize) {
+	if (hi - lo) > 1 {
+		var m int = (lo + hi) / 2
+		var w int = lo + hi - m
+		wsort(arr, lo, m, w)
+		for (w - lo) > 2 {
+			var n int = w
+			w = (lo + n + 1) / 2
+			wsort(arr, w, n, lo)
+			wmerge(arr, lo, lo + n - w, n, hi, w)
 		}
-		for i in w..lo {
-			let mut j = i;
-			while j < hi && arr[j] < arr[j - 1] {
-				arr.swap(j, j - 1);
-				j += 1;
+		for i := w; i > lo; i-- {
+			for j := i; (j < hi) && (arr[j] < arr[j - 1]); j++ {
+				swap(arr, j, j - 1)
 			}
 		}
 	}
 }
 
-fn permute(l: &mut [Vec<u8>], n: usize, m: usize, pos: usize) {
+fn permute(l [][]byte, n: usize, m: usize, pos: usize) {
 	if n == 0 {
-		return;
+		return
 	}
-	let mut size = 1;
-	for _i in 0..n-1 {
-		size *= m;
+	var size int = 1
+	for i := 0; i < n - 1; i++ {
+		size *= m
 	}
-	for i in 0..m {
-		for j in 0..size {
-			l[i * size + j][pos] = ('z' as usize - i) as u8;
+	for i := 0; i < m; i++ {
+		for j := 0; j < size; j++ {
+			l[i * size + j][pos] = byte('z' - i)
 		}
-		permute(&mut l[(i * size)..], n - 1, m, pos + 1);
+		permute(l[i * size:], n - 1, m, pos + 1);
 	}
 }
 
-fn gen_array(n: usize, m: usize, size: &mut usize) -> Vec<String> {
-	*size = 1;
-	for _i in 0..n {
+fn gen_array(n: usize, m: usize, size: &mut usize) []string {
+	*size = 1
+	for i := 0; i < n; i++ {
 		*size *= m
 	}
-	let mut l: Vec<Vec<u8>> = Vec::new(); //make([][]byte, *size)
-	for i in 0..*size {
-		l[i] = Vec::new() //make([]byte, n)
+	var l [][]byte = make([][]byte, *size)
+	for i := 0; i < *size; i++ {
+		l[i] = make([]byte, n)
 	}
-	let t0 = std::time::Instant::now();
-	permute(&mut l, n, m, 0);
-	let t1 = std::time::Instant::now();
-	println!("[info] permute: {} ns\n", (t1 - t0).as_nanos());
-	let mut result: Vec<String> = Vec::new(); //make([]string, *size)
-	for i in 0..*size {
-		result[i] = l[i].iter().map(|u8| *u8 as char).collect();
+	var t0 time.Time = time.Now()
+	permute(l, n, m, 0);
+	var t1 time.Time = time.Now()
+	fmt.Printf("[info] permute: %v ns\n", t1.Sub(t0).Nanoseconds())
+	var result []string = make([]string, *size)
+	for i := 0; i < *size; i++ {
+		// fmt.Println(string(l[i]));
+		result[i] = string(l[i])
 	}
 	return result
 }
 
-fn verify_array(l: Vec<String>) -> bool {
-	for i in 1..l.len() {
+fn verify_array(l: &[String]) {
+	for i := 1; i < len(l); i++ {
 		if l[i - 1] > l[i] {
-			return false
+			panic!("badness");
 		}
 	}
 	return true
 }
 
-pub fn main() {
-	let mut size = 0usize;
-	let t0 = std::time::Instant::now();
-	let mut l = gen_array(6, 18, &mut size);
-	let t1 = std::time::Instant::now();
-	imsort(&mut l, 0, size);
-	let t2 = std::time::Instant::now();
-	println!("[info] gen_array: {} ns\n", (t1 - t0).as_nanos());
-	println!("[info] sort: {} ns\n", (t2 - t1).as_nanos());
-	assert(verify_array(l))
+fn main() {
+	let mut size = 0;
+	let t0 time.Time = time.Now();
+	let l []string = gen_array(6, 18, &mut size);
+	let t1 time.Time = time.Now();
+	imsort(l, 0, size);
+	var t2 time.Time = time.Now();
+	fmt.Printf("[info] gen_array: %v ns\n", t1.Sub(t0).Nanoseconds());
+	fmt.Printf("[info] sort: %v ns\n", t2.Sub(t1).Nanoseconds());
+	verify_array(l);
 }
